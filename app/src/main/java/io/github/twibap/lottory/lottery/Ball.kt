afc33eeee470d978827f93ec00ea8d357dfec949
유태경 <tkyoo@spidor.co.kr>
2021-07-28 12:23:06 +0900

    중복 제거 및 배열 출력

--- app/src/main/java/io/github/twibap/lottory/lottery/Ball.kt
+++ app/src/main/java/io/github/twibap/lottory/lottery/Ball.kt
@@ -1,6 +1,6 @@
 package io.github.twibap.lottory.lottery
 
-class Ball: Comparable<Ball> {
+class Ball(numbers: Array<Ball?>) : Comparable<Ball> {
 
     var value: Int
 
@@ -8,8 +8,8 @@ class Ball: Comparable<Ball> {
         var randomVal: Double
         do {
             randomVal = Math.random();
-            this.value = (randomVal* MAX_VALUE).toInt() + 1
-        } while (value !in MIN_VALUE..MAX_VALUE)
+            this.value = (randomVal* MAX_VALUE).toInt() + MIN_VALUE
+        } while (value in numbers.map { ball -> ball?.value })
     }
 
     companion object {
@@ -19,6 +19,10 @@ class Ball: Comparable<Ball> {
 
     override fun compareTo(other: Ball): Int {
         return this.value - other.value
+    }
+
+    override fun toString(): String {
+        return "($value)"
     }
 
 }
--- app/src/main/java/io/github/twibap/lottory/lottery/Lottery.kt
+++ app/src/main/java/io/github/twibap/lottory/lottery/Lottery.kt
@@ -1,28 +1,20 @@
 package io.github.twibap.lottory.lottery
 
-class Lottery: Comparable<Lottery> {
+class Lottery {
 
-    val num1: Ball = Ball()
-    val num2: Ball = Ball()
-    val num3: Ball = Ball()
-    val num4: Ball = Ball()
-    val num5: Ball = Ball()
-    val num6: Ball = Ball()
-    val numBonus: Ball = Ball()
+    // last one is bonus number for 2nd
+    private val numbers: Array<Ball?> = arrayOfNulls(6)
+    private val bonus: Ball
 
-    override fun toString(): String {
-        return "${num1.value} / ${num2.value} / ${num3.value} / ${num4.value} / ${num5.value} / ${num6.value} + ${numBonus.value}"
+    init {
+        for (i in numbers.indices)
+            numbers[i] = Ball(numbers)
+        bonus = Ball(numbers)
+        numbers.sort();
     }
 
-    /**
-     * if return 0 is WINNER
-     */
-    override fun compareTo(other: Lottery): Int {
-        return this.num1.compareTo(other.num1) or
-                this.num2.compareTo(other.num2) or
-                this.num3.compareTo(other.num3) or
-                this.num4.compareTo(other.num4) or
-                this.num5.compareTo(other.num5) or
-                this.num6.compareTo(other.num6)
+    override fun toString(): String {
+        this.numbers.toString()
+        return "${numbers.contentToString()} + $bonus"
     }
 }
